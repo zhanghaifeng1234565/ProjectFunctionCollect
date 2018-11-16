1ed7c72435c8625604359b3b13cf629a9626fa96
iOS <ios@iosdevdeMacBook-Air.local>
2018-11-16 15:18:34 +0800

    更新
    更新

--- YMOCCodeStandard/YMOCCodeStandard/YMClasses/FirstModule/FirstModuleModel/YMHomeModel.h
+++ YMOCCodeStandard/YMOCCodeStandard/YMClasses/FirstModule/FirstModuleModel/YMHomeModel.h
@@ -21,16 +21,4 @@
 @property (nonatomic, copy) NSString *time;
 
 @end
-/*
- 
- "active_img" = "http://www.kys18.com/app/image/money.png";
- "active_introduce" = "\U9080\U8bf7\U597d\U53cb\U5e76\U6210\U529f\U6ce8\U518c\U5171\U4eab\U540d\U533b,\U53cc\U65b9\U90fd\U5c06\U83b7\U5f973\U5143\U7ea2\U5305,\U53ef\U76f4\U63a5\U4f53\U73b0,\U5feb\U6765\U6ce8\U518c\U5427";
- "active_title" = "\U9080\U8bf7\U597d\U53cb\U9886\U7ea2\U5305\U5566";
- "active_url" = "";
- money = "3\U5143";
- msg = "\U6d3b\U52a8\U4fe1\U606f";
- "share_url" = "http://www.kys18.com/mobile/active.php";
- status = 1;
- time = "2018-09-17 17:30:23";
- 
- */
+
